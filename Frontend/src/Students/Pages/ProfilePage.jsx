f63f7c7730f26d86e68ef5f0bd7e431eeef668fc
minhajkc <minhajkc00@gmail.com>
2024-09-09 16:30:30 +0530

    completed payment

--- Frontend/src/Students/Pages/ProfilePage.jsx
+++ Frontend/src/Students/Pages/ProfilePage.jsx
@@ -34,7 +34,7 @@ const StudentPortal = () => {
 
     if (loading) {
         return <div className="flex items-center justify-center h-screen bg-gray-100">
-            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-800"></div>
         </div>;
     }
 
